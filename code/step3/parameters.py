e612798ac8e4b16f0da574903d70216b9d22b19c
Daniel Beasley <dan.abxy@gmail.com>
2017-04-11 14:18:54 -0400

    remove old code

--- code/step3/parameters.py
+++
@@ -1,8 +0,0 @@
-# -----Grid Search Parameters-----
-GRID_SEARCH_PARAM = {
-    'LogisticRegression': {'C': [50.0, 75.0, 100.0, 125.0, 150.0, 200.0],
-                           'solver': ['newton-cg', 'lbfgs'],
-			   'fit_intercept': [True, False],
-			   'max_iter': [50, 100, 150, 200],
-			   'tol': [1e-2, 1e-3, 1e-4, 1e-5] }
-}
